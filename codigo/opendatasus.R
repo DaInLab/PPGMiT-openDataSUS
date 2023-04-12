3d27fa4a10c6ff370c207555c08b44f9f6c00ffd
Your Namejpalbino <jp.albino@unesp.br>
2023-04-12 10:48:01 -0300

    Atualizando para grandes arquivos: 2023-04-12

--- codigo/opendatasus.R
+++ codigo/opendatasus.R
@@ -7,13 +7,25 @@
 # Fonte : https://opendatasus.saude.gov.br/dataset/srag-2021-a-2023
 
 
+<<<<<<< HEAD
 # Import packages
 #library(ggplot2) # graficos
+=======
+# Carregando as bibliotecas necessárias
+if (!("ggplot2") %in% installed.packages()) install.packages("ggplot2")   
+if (!("dplyr") %in% installed.packages()) install.packages("dplyr")
+if (!("readr") %in% installed.packages()) install.packages("readr")
+library(ggplot2) # graficos
+>>>>>>> 9cb3c0a (Atualizando via MacOsx: 2023-04-11.)
 library(dplyr) # manipulacao de dados
 library(readr) 
 
 # Diretorio da Trabalho
+<<<<<<< HEAD
 setwd('D:/GoogleDrive/Mestrado_unesp/PPGMiT-openDataSUS')
+=======
+#setwd('D:/GoogleDrive/Mestrado_unesp/openDataSUS')
+>>>>>>> 9cb3c0a (Atualizando via MacOsx: 2023-04-11.)
 
 # Importacao dos arquivos anuais : INFLUD* 2021, 2022 e 2023 (2.339.381)
 df_opendatasus<-list.files(path="./dados",pattern="*.csv", full.names = TRUE) %>%
@@ -28,8 +40,13 @@ summary(df_opendatasus)
 
 # Filtro - 78-Classificacao Final do caso (5-SRAG por COVID-19)
 df_covid <- df_opendatasus %>% filter(CLASSI_FIN == "5")
+<<<<<<< HEAD
 
 
+=======
+View(df_covid)
+summary(df_covid)
+>>>>>>> 9cb3c0a (Atualizando via MacOsx: 2023-04-11.)
 
 
 
